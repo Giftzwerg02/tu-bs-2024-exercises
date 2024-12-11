cb169a2a1ccba5794657d3ebfbbe586c21e99e0d
Giftzwerg02 <chessplayer1@gmx.at>
2024-12-11 16:18:49 +0100

    update

---
+++ giftzwerg02/exercise-1/task-2-again/supervisor.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+
+int main(int argc, char** argv) {
+  return EXIT_SUCCESS;
+}
+
+int argparse(int argc, char** argv) {
+  
+}
--- giftzwerg02/flake.nix
+++ giftzwerg02/flake.nix
@@ -1,24 +1,29 @@
 {
   description = "Go proj flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";  
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default =
+          pkgs.mkShell
           {
-            buildInputs = with pkgs;[
-				gcc
-				gnumake
-				gdb
-				linux-manual 
-				man-pages 
-				man-pages-posix
+            buildInputs = with pkgs; [
+              gcc
+              gnumake
+              gdb
+              linux-manual
+              man-pages
+              man-pages-posix
             ];
           };
       }
